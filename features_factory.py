b20295aea703daec7e183bde1bc3bf808c01f611
Mohamed Sayed <ms10596@gmail.com>
2019-02-09 20:43:24 +0200

    feat: adjectives size

--- features_factory.py
+++ features_factory.py
@@ -1,6 +1,6 @@
 from statistics import mean
 
-from nltk import word_tokenize, sent_tokenize
+from nltk import word_tokenize, sent_tokenize, pos_tag
 
 from load import load
 
@@ -16,7 +16,10 @@ class FeaturesFactory:
     def avg_word_length(self):
         return mean([len(i) for i in word_tokenize(self.body)])
 
+    def adj(self):
+        return len([i for i in pos_tag(word_tokenize(self.body)) if i[1] == 'JJ'])
+
 
 if __name__ == '__main__':
     x = FeaturesFactory(load()[0])
-    print(x.avg_word_length())
+    print(x.adj())
