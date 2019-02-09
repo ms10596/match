7276a15d63798b9e0ec9f5953c2b3692786419b2
Mohamed Sayed <ms10596@gmail.com>
2019-02-09 17:11:20 +0200

    docs: cleaning data from non utf characters

--- Text.py
+++
@@ -1,4 +0,0 @@
-class Text:
-    def __init__(self, text, tag):
-        self.text = text
-        self.tag = tag
--- load.py
+++ load.py
@@ -1,16 +1,25 @@
 import os
 
-from Text import Text
+from nltk import Text, word_tokenize
+
+
+def string_to_text(s):
+    return Text(word_tokenize(s))
 
 
 def load():
+    """
+    :return: list of tuples(nltk.text, tag like A1, A2,...etc)
+
+    """
     texts = []
     for tag in ['A1', 'A2', 'B1', 'B2', 'C1']:
         files = os.listdir('CEFR/' + tag)
         for i in files:
             f = open('CEFR/' + tag + '/' + i)
-            texts.append(Text(text=f.read(), tag=tag))
+            # print(f.name)
+            texts.append((string_to_text(f.read()), tag))
     return texts
 
 
-print(load()[0].tag)
+print(load()[0])
