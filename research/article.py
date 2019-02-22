bf3cf207a3b3f93fd55bc622afbc6a09fc3268b7
Mohamed Sayed <ms10596@gmail.com>
2019-02-22 19:35:06 +0200

    feat: reduced features

--- research/article.py
+++ research/article.py
@@ -11,3 +11,29 @@ class Article:
 
         self.word_tags = pos_tag(self.words)
         self.frequencies = FreqDist([i[1] for i in self.word_tags])
+        self.reduced_frequencies = {}
+
+    def get_frequency(self, feature_name):
+        try:
+            return int(self.frequencies.get(feature_name))
+        except TypeError:
+            return 0
+
+    def features_reduction(self):
+        self.reduced_frequencies['adjectives'] = self.get_frequency('JJ') + self.get_frequency(
+            'JJR') + self.get_frequency('JJS')
+        self.reduced_frequencies['adverbs'] = self.get_frequency('RB') + self.get_frequency('RBR') + self.get_frequency(
+            'RBS')
+        self.reduced_frequencies['articles'] = self.get_frequency('DT')
+        self.reduced_frequencies['conjunctions'] = self.get_frequency('CC')
+        self.reduced_frequencies['interjections'] = self.get_frequency('UH')
+        self.reduced_frequencies['nouns'] = self.get_frequency('NN') + self.get_frequency('NNS') + self.get_frequency(
+            'NNP') + self.get_frequency('NNPS')
+        self.reduced_frequencies['numerals'] = self.get_frequency('CD')
+        self.reduced_frequencies['past_part'] = self.get_frequency('VBN')
+        self.reduced_frequencies['preposition'] = self.get_frequency('IN')
+        self.reduced_frequencies['pronouns'] = self.get_frequency('PRP') + self.get_frequency('PRP$')
+        self.reduced_frequencies['punctuation'] = self.get_frequency('``') + self.get_frequency(
+            '\'\'') + self.get_frequency('(') + self.get_frequency(')') + self.get_frequency(',') + self.get_frequency(
+            '--') + self.get_frequency('.') + self.get_frequency(':')
+        self.reduced_frequencies['symbols'] = self.get_frequency('SYM')
