8c64055b09438f2b79f5b809b7d33821484ed9ad
Mohamed Sayed <ms10596@gmail.com>
2019-04-14 11:16:26 +0200

    feat: lexile score close to the real one.

--- Lexile/lexile.py
+++ Lexile/lexile.py
@@ -1,4 +1,4 @@
-import math
+from math import log
 import re
 from collections import defaultdict
 from statistics import mean
@@ -20,10 +20,10 @@ class Lexile:
                 self.words.remove(i)
 
     def mean_sentence_length(self):
-        return mean([len(i) for i in self.sentences])
+        return mean([len(word_tokenize(i)) for i in self.sentences])
 
     def mean_word_freq(self):
-        return math.log10(mean([self.wordsFreq[i] for i in self.words]))
+        return log(mean([self.wordsFreq[i] for i in self.words]), 10)
 
     def word_count(self):
         return len(self.words)
@@ -44,12 +44,19 @@ class Lexile:
     def get_score(self):
         sentence_len = self.mean_sentence_length()
         word_frequency = self.mean_word_freq()
-        return 582 + (1768 * sentence_len - 386 * word_frequency)
+        return 582 + 1768 * log(sentence_len, 10) - 386 * word_frequency
 
 
 if __name__ == "__main__":
     l = Lexile(
-        """The Lexile Analyzer uses an algorithm to evaluate the reading demand — or text complexity — of books, articles and other materials. The Lexile Analyzer measures the complexity of the text by breaking down the entire piece and studying its characteristics, such as sentence length and word frequency, which represent the syntactic and semantic challenges that the text presents to a reader. The outcome is the text complexity, expressed as a Lexile measure, along with information on the word count, mean sentence length and mean log frequency. Generally, longer sentences and words of lower frequency lead to higher Lexile measures; shorter sentences and words of higher frequency lead to lower Lexile measures. Texts such as lists, recipes, poetry and song lyrics are not analyzed because they lack conventional punctuation.""")
+        """The Lexile Analyzer uses an algorithm to evaluate the reading demand — or text complexity — of books, 
+        articles and other materials. The Lexile Analyzer measures the complexity of the text by breaking down the 
+        entire piece and studying its characteristics, such as sentence length and word frequency, which represent 
+        the syntactic and semantic challenges that the text presents to a reader. The outcome is the text complexity, 
+        expressed as a Lexile measure, along with information on the word count, mean sentence length and mean log 
+        frequency. Generally, longer sentences and words of lower frequency lead to higher Lexile measures; shorter 
+        sentences and words of higher frequency lead to lower Lexile measures. Texts such as lists, recipes, 
+        poetry and song lyrics are not analyzed because they lack conventional punctuation.""")
     print("Lexile measure: ", l.get_score())
     print("Mean Sentence Length: ", l.mean_sentence_length())
     print("Mean Log Word Frequency: ", l.mean_word_freq())
