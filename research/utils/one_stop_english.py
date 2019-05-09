e4b105d439a3aa124a57f9697e3751b79c848bd9
Mohamed Sayed <ms10596@gmail.com>
2019-05-09 15:42:57 +0200

    feat: read text

--- research/utils/one_stop_english.py
+++ research/utils/one_stop_english.py
@@ -9,8 +9,15 @@ class OneStopEnglish:
         articles = []
         tags = []
         for i in ['Ele-Txt', 'Int-Txt', 'Adv-Txt']:
-            files = os.listdir(os.path.join(os.path.abspath(os.curdir), x, i))
-            print(files)
+            files = [i for i in os.listdir(os.path.join(os.path.abspath(os.curdir), x, i)) if not i.startswith('.')]
+            # print(len(files))
+            for j in files:
+                f = open(os.path.join(os.path.abspath(os.curdir), x, i,j))
+                print(f)
+                articles.append(f.read())
+                tags.append(i)
+        # print(len(articles))
+        # print(len(tags))
 
 
 OneStopEnglish().load_raw()
