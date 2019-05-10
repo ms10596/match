41f066de6b1ff573c07526ab32ba2019492b0b80
Mohamed Sayed <ms10596@gmail.com>
2019-05-10 15:35:48 +0200

    refactor: code styling

--- research/utils/one_stop_english.py
+++ research/utils/one_stop_english.py
@@ -1,5 +1,6 @@
 import os
 from random import shuffle
+
 x = 'Corpus/OneStopEnglishCorpus/Texts-SeparatedByReadingLevel'
 
 
@@ -8,20 +9,17 @@ class OneStopEnglish:
         self.articles = []
         self.tags = []
         self.load_raw()
-        self._shuffle()
+        self.__shuffle()
+
     def load_raw(self):
         for i in ['Ele-Txt', 'Int-Txt', 'Adv-Txt']:
             files = [i for i in os.listdir(os.path.join(os.path.abspath(os.curdir), x, i)) if not i.startswith('.')]
             for j in files:
-                f = open(os.path.join(os.path.abspath(os.curdir), x, i,j))
+                f = open(os.path.join(os.path.abspath(os.curdir), x, i, j))
                 self.articles.append(f.read())
                 self.tags.append(['Ele-Txt', 'Int-Txt', 'Adv-Txt'].index(i))
-    def _shuffle(self):
+
+    def __shuffle(self):
         zipped = list(zip(self.articles, self.tags))
         shuffle(zipped)
         self.articles, self.tags = zip(*zipped)
-
-
-x = OneStopEnglish()
-
-
