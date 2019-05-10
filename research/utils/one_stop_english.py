80d70f2038b915cee825ab06e60e899d6554e2d2
Mohamed Sayed <ms10596@gmail.com>
2019-05-10 15:27:47 +0200

    feat: shuffle data

--- research/utils/one_stop_english.py
+++ research/utils/one_stop_english.py
@@ -1,23 +1,27 @@
 import os
-
+from random import shuffle
 x = 'Corpus/OneStopEnglishCorpus/Texts-SeparatedByReadingLevel'
 
 
 class OneStopEnglish:
-
+    def __init__(self):
+        self.articles = []
+        self.tags = []
+        self.load_raw()
+        self._shuffle()
     def load_raw(self):
-        articles = []
-        tags = []
         for i in ['Ele-Txt', 'Int-Txt', 'Adv-Txt']:
             files = [i for i in os.listdir(os.path.join(os.path.abspath(os.curdir), x, i)) if not i.startswith('.')]
-            # print(len(files))
             for j in files:
                 f = open(os.path.join(os.path.abspath(os.curdir), x, i,j))
-                print(f)
-                articles.append(f.read())
-                tags.append(i)
-        # print(len(articles))
-        # print(len(tags))
+                self.articles.append(f.read())
+                self.tags.append(['Ele-Txt', 'Int-Txt', 'Adv-Txt'].index(i))
+    def _shuffle(self):
+        zipped = list(zip(self.articles, self.tags))
+        shuffle(zipped)
+        self.articles, self.tags = zip(*zipped)
 
 
-OneStopEnglish().load_raw()
+x = OneStopEnglish()
+
+
