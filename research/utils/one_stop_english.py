0382c93065cb73813b6de89057b8704993377b0a
Mohamed Sayed <ms10596@gmail.com>
2019-05-11 13:30:20 +0200

    feat: load parallel sentences

--- research/utils/one_stop_english.py
+++ research/utils/one_stop_english.py
@@ -1,7 +1,8 @@
-import os
+from os import path, listdir, curdir
 from random import shuffle
 
-x = 'Corpus/OneStopEnglishCorpus/Texts-SeparatedByReadingLevel'
+text_path = path.join(path.abspath(curdir), 'Corpus/OneStopEnglishCorpus/Texts-SeparatedByReadingLevel')
+sentences_path = path.join(path.abspath(curdir), 'Corpus/OneStopEnglishCorpus/Sentence-Aligned')
 
 
 class OneStopEnglish:
@@ -13,9 +14,9 @@ class OneStopEnglish:
 
     def load_raw(self):
         for i in ['Ele-Txt', 'Int-Txt', 'Adv-Txt']:
-            files = [i for i in os.listdir(os.path.join(os.path.abspath(os.curdir), x, i)) if not i.startswith('.')]
+            files = [i for i in listdir(path.join(text_path, i)) if not i.startswith('.')]
             for j in files:
-                f = open(os.path.join(os.path.abspath(os.curdir), x, i, j))
+                f = open(path.join(text_path, i, j))
                 self.articles.append(f.read())
                 self.tags.append(['Ele-Txt', 'Int-Txt', 'Adv-Txt'].index(i))
 
@@ -23,3 +24,42 @@ class OneStopEnglish:
         zipped = list(zip(self.articles, self.tags))
         shuffle(zipped)
         self.articles, self.tags = zip(*zipped)
+
+    @staticmethod
+    def load_advanced_elementary():
+        f = open(path.join(sentences_path, 'ADV-ELE.txt'))
+        all = [i.strip().split('\n') for i in f.read().split("*******")]
+        advanced = [i[0] for i in all if len(i) == 2]
+        elementary = [i[1] for i in all if len(i) == 2]
+        return advanced, elementary
+
+    @staticmethod
+    def load_advanced_intermediate():
+        f = open(path.join(sentences_path, 'ADV-INT.txt'))
+        all = [i.strip().split('\n') for i in f.read().split("*******")]
+        advanced = [i[0] for i in all if len(i) == 2]
+        intermediate = [i[1] for i in all if len(i) == 2]
+        return advanced, intermediate
+
+    @staticmethod
+    def load_intermediate_elementary():
+        f = open(path.join(sentences_path, 'ELE-INT.txt'))
+        all = [i.strip().split('\n') for i in f.read().split("*******")]
+        elementary = [i[0] for i in all if len(i) == 2]
+        intermediate = [i[1] for i in all if len(i) == 2]
+        return intermediate, elementary
+
+
+# x = OneStopEnglish()
+# a, b = x.load_advanced_elementary()
+# print(a[1])
+# print(b[1])
+# print(len(a), len(b))
+# a, b = x.load_advanced_intermediate()
+# print(a[1])
+# print(b[1])
+# print(len(a), len(b))
+# a, b = x.load_intermediate_elementary()
+# print(a[1])
+# print(b[1])
+# print(len(a), len(b))
